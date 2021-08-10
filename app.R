32eae9336d400df9deea49d5155047ab83c712e2
thiagojk <thiagojk33@gmail.com>
2021-08-10 11:24:07 -0300

    1

--- app.R
+++ app.R
@@ -192,6 +192,7 @@ server <- function(input, output, session){
   })
   ##################################################################################
   # Barra semana
+  # 
   
   
   output$barra_semana <- renderPlot({
